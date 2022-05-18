16c4b4b4dd62a869de2e34156645de4f4fb0c017
Svork7 <93423288+Svork7@users.noreply.github.com>
2022-05-18 14:05:12 +0300

    Задача 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,24 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  let d = Math.pow(b,2) - 4*a*c;
+  if (d === 0) {
+    arr[0] = (-b + Math.sqrt(d)) / 2*a;
+    console.log(`Корень равен: ${arr[0]}`);
+  }  
+  else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / 2*a;
+    arr[1] = (-b - Math.sqrt(d)) / 2*a;
+    console.log (`Корни равны: ${arr[0]}, ${arr[1]}`);
+  } 
+  else {
+    console.log('Корней нет');
+  }
+  return arr;
+};
   // код для задачи №1 писать здесь
-  return arr; // array
-}
+   // array
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
