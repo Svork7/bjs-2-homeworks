138fa63ce0cc9580252fff2f5b413c1d927ae3bc
Eugene Mikhailov <svork@ya.ru>
2022-08-09 09:51:15 +0300

    done 2/2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result; 
+  return result = arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
 }
+
+compareArrays([1, 2, 3], [1, 2, 3])
+compareArrays([1, 2, 3], [3, 2, 1])
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10); // array
 }
